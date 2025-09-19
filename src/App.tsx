984b5e48276b695382931ff7a74549ce58090d61
Sandeep <sandipsharma2445@gmail.com>
2025-09-19 12:19:56 +0530

    tailwind css configured

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // TypeScript automatically infers a function component as a function
 // that returns a JSX.Element. If we want to make it explicit, we can use React.FC.
 const App: React.FC = () => {
-    return <h1>Recipe finder</h1>
+    return <h1 className="text-3xl text-red-500">Recipe finder</h1>
 }
 
 export default App;
